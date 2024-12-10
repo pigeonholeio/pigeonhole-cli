a42c7e9a54d87a3c17483e176b7fc9d1d2001243
Rhys <rhys@rhysevans.co.uk>
2024-12-10 21:56:00 +0000

    updated things

--- src/cmd/keys/keysInit.go
+++ src/cmd/keys/keysInit.go
@@ -12,8 +12,8 @@ var KeysInitCmd = &cobra.Command{
 	Short: "Force generate and push a new set of GPG keys - this will delete all your existing keys",
 	Long:  `Use for the first time or if you want to wipe out all your keys.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Generating new GPG key as %s...")
 		claims, _ := common.DecodePigeonHoleJWT()
+		fmt.Printf("Generating new GPG key as %s...", claims["preferred_username"].(string))
 		complete, err := common.GenerateKeys(claims["name"].(string), claims["preferred_username"].(string))
 		if complete {
 			fmt.Println("done!")
